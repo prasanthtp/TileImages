68f59d2243d88f702d58ce8d89e27826415cdab3
CORIN\ppillai <Prasanth.Pillai@coringroup.com>
2019-04-05 14:19:14 +1100

    blah

--- Program.cs
+++ Program.cs
@@ -9,18 +9,34 @@ namespace delete68
     class Program
     {
         static void Main(string[] args)
-        {
-            var files = new string[] { @"C:\Temp\1.png", @"C:\Temp\2.png", @"C:\Temp\3.png", @"C:\Temp\4.png", @"C:\Temp\5.png", @"C:\Temp\6.png", @"C:\Temp\7.png", @"C:\Temp\8.png", @"C:\Temp\9.png" };
+        { 
 
-            Bitmap img = CombineBitmap(files);
-            Bitmap resized = new Bitmap(img, new Size(800, 800));
+            var files = new string[] { @"C:\Temp\35_20.png", @"C:\Temp\35_25.png", @"C:\Temp\35_30.png", @"C:\Temp\40_20.png", @"C:\Temp\40_25.png", @"C:\Temp\40_30.png", @"C:\Temp\45_20.png", @"C:\Temp\45_25.png", @"C:\Temp\45_30.png" };
+            string back = @"C:\Temp\back.png";
 
-            resized.Save(@"C:\Temp\final.png", ImageFormat.Png);
+            Bitmap img = CombineBitmap(files, back);
+         //   Bitmap resized = new Bitmap(img, new Size(962, 962));
+            img.Save(@"C:\Temp\final.png", ImageFormat.Png);
 
 
 
         }
-            public static System.Drawing.Bitmap CombineBitmap(string[] files)
+
+        private void DrawBitmapWithBorder(Bitmap bmp, Point pos, Graphics g)
+        {
+            const int borderSize = 20;
+
+            using (Brush border = new SolidBrush(Color.White /* Change it to whichever color you want. */))
+            {
+                g.FillRectangle(border, pos.X - borderSize, pos.Y - borderSize,
+                    bmp.Width + borderSize, bmp.Height + borderSize);
+            }
+
+            g.DrawImage(bmp, pos);
+        }
+
+
+        public static System.Drawing.Bitmap CombineBitmap(string[] files, string backgroundImage)
         {
             //read all images into memory
             List<System.Drawing.Bitmap> images = new List<System.Drawing.Bitmap>();
@@ -52,9 +68,16 @@ namespace delete68
                 //create a bitmap to hold the combined image
                 finalImage = new System.Drawing.Bitmap(width, height);
 
+
+                System.Drawing.Bitmap backbmp = new System.Drawing.Bitmap(backgroundImage);
+              
+
+
                 //get a graphics object from the image so we can draw on it
                 using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(finalImage))
                 {
+                    g.DrawImage(backbmp, new System.Drawing.Rectangle(0, 0, finalImage.Width, finalImage.Height));
+
                   
 
                     //go through each image and draw it on the final image
@@ -63,9 +86,16 @@ namespace delete68
                     int j = 0;
                     foreach (System.Drawing.Bitmap image in images)
                     {
-                      
+
+                        g.DrawLine(new Pen(Brushes.Gray, 5), new Point(offsetx, offsety), new Point(offsetx, offsety+ image.Height));
+                        g.DrawLine(new Pen(Brushes.Gray, 5), new Point(offsetx, offsety), new Point(offsetx+ image.Width, offsety));
+                        g.DrawLine(new Pen(Brushes.Gray, 5), new Point(offsetx, offsety + image.Height), new Point(offsetx + image.Width, offsety + image.Height));
+                        g.DrawLine(new Pen(Brushes.Gray, 5), new Point(offsetx + image.Width, offsety), new Point(offsetx + image.Width, offsety + image.Height));
 
                         g.DrawImage(image, new System.Drawing.Rectangle(offsetx, offsety, image.Width, image.Height));
+
+                        
+
 
                         offsetx += image.Width;
                         if (j != 0 && j % 3 == 2)
